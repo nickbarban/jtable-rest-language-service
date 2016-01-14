f2f38be5efc65baca8ac496653147745e738d212
Nick Barban <nn.barban@gmail.com>
2016-01-14 14:48:45 +0200

    Add H2 console

--- src/main/java/com/barban/configuration/SecurityConfiguration.java
+++ src/main/java/com/barban/configuration/SecurityConfiguration.java
@@ -9,7 +9,17 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity httpSecurity) throws Exception {
-		httpSecurity.authorizeRequests().antMatchers("/").permitAll();
+		httpSecurity.authorizeRequests().antMatchers("/").permitAll().and().authorizeRequests()
+				.antMatchers("/console/**").permitAll();
+
+		httpSecurity.csrf().disable();
+		httpSecurity.headers().frameOptions().disable();
+		;
 	}
 
 }
+
+/*
+ * Driver Class org.h2.Driver JDBC URL jdbc:h2:mem:testdb User Name sa Password
+ * <blank>
+ */
