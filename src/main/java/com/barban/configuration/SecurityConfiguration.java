db61e4fff7eadc254f19b1acb61167a4bcdb2442
Nick Barban <nn.barban@gmail.com>
2016-01-14 15:42:17 +0200

    Refactor app properties

--- src/main/java/com/barban/configuration/SecurityConfiguration.java
+++ src/main/java/com/barban/configuration/SecurityConfiguration.java
@@ -19,9 +19,3 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 }
 
-/*
- * Driver Class org.h2.Driver 
- * JDBC URL jdbc:h2:mem:testdb 
- * User Name sa 
- * Password
- */
