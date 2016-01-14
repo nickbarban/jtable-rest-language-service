332eb4fff8324f52a4bb15b7f0f8e1d47544bc91
Nick Barban <nn.barban@gmail.com>
2016-01-14 14:03:46 +0200

    Init

---
+++ src/main/java/com/barban/JtableRestLanguageServiceApplication.java
@@ -0,0 +1,12 @@
+package com.barban;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JtableRestLanguageServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JtableRestLanguageServiceApplication.class, args);
+	}
+}
---
+++ src/test/java/com/barban/JtableRestLanguageServiceApplicationTests.java
@@ -0,0 +1,18 @@
+package com.barban;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = JtableRestLanguageServiceApplication.class)
+@WebAppConfiguration
+public class JtableRestLanguageServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
